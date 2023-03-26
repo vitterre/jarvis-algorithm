b17d21c8ccc0aa7b41cf3d61ec9408848f1038be
vitterre <bagatovbb@gmail.com>
2023-03-26 14:03:06 +0300

    tests: add one test for Jarvis algorithm

--- generator/generator.py
+++ generator/generator.py
@@ -1,45 +1,21 @@
-import csv
-import random
+import numpy as np
 from scipy.spatial import ConvexHull
+import csv
 
 JAVA_TEST_RESOURCES_PATH = "../jarvis/src/test/resources/"
 
-# Generate test data
-def generate_test_data(n_points):
-    points = set()
-    while len(points) < n_points:
-        x = random.randint(-100, 100)
-        y = random.randint(-100, 100)
-        points.add((x, y))
-    return list(points)
-
-
-test_sets = []
 for i in range(10):
-    n_points = random.randint(10, 100)
-    points = generate_test_data(n_points)
-    test_sets.append(points)
+    points = np.random.randint(-100, 100, size=(100, 2))
 
-
-# Calculate expected results
-def convex_hull_algorithm(points):
     hull = ConvexHull(points)
-    return hull.vertices.tolist()
+    convex_hull_points = points[hull.vertices]
 
+    with open(JAVA_TEST_RESOURCES_PATH + f"test_data_{i}.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["x", "y"])
+        writer.writerows(points)
 
-# Write test data and expected results
-for i, points in enumerate(test_sets):
-    # Write test results
-    with open(JAVA_TEST_RESOURCES_PATH + f'test_data_{i}.csv', 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(['x', 'y'])
-        for point in points:
-            writer.writerow(point)
-
-    # Write expected results
-    with open(JAVA_TEST_RESOURCES_PATH + f'expected_result_{i}.csv', 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(['index'])
-        hull_indices = convex_hull_algorithm(points)
-        for index in hull_indices:
-            writer.writerow([index])
+    with open(JAVA_TEST_RESOURCES_PATH + f"expected_output_{i}.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["x", "y"])
+        writer.writerows(convex_hull_points)
