52b7dbdf68fe69eb305bc944b8d38ccbc8975734
vitterre <bagatovbb@gmail.com>
2023-03-26 12:22:41 +0300

    feat: add Jarvis algorithm

--- jarvis/src/main/java/ru/itis/ads/jarvis/Main.java
+++ jarvis/src/main/java/ru/itis/ads/jarvis/Main.java
@@ -1,7 +1,16 @@
 package ru.itis.ads.jarvis;
 
+import ru.itis.ads.jarvis.point.Point;
+
+import java.util.List;
+
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Jarvis algorithm!");
+
+		final List<Point> points = List.of(new Point(0, 3), new Point(2, 2), new Point(1, 1), new Point(2, 1),
+						new Point(3, 0), new Point(0, 0), new Point(3, 3));
+		final List<Point> hull = JarvisAlgorithm.createConvexHull(points);
+		System.out.println(hull);
 	}
 }
