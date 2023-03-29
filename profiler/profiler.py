a0a1ff5fc123c3dbcda919bcc19855049b7d27c9
vitterre <bagatovbb@gmail.com>
2023-03-29 13:17:25 +0300

    chore(profiler): add titles to both graphs

--- profiler/profiler.py
+++ profiler/profiler.py
@@ -9,7 +9,7 @@ with open('jarvis/log.csv') as f:
     for line in f:
         values = tuple(map(int, line.strip().split(',')))
         array_size, execution_time, iteration_count, solution_size = values[2], values[0], values[1], values[3]
-        
+
         array_sizes.append(array_size)
         execution_times.append(execution_time)
         iteration_counts.append(iteration_count)
@@ -21,11 +21,13 @@ ax.plot(array_sizes, iteration_counts, label='Iteration Count')
 ax.set_xlabel('Array Size')
 ax.set_ylabel('Time/Iterations')
 ax.legend()
+plt.title("Execution time and iteration count depending on array size")
 plt.show()
 
 plt.plot(array_sizes, solution_sizes)
 plt.yscale('log')
 plt.xlabel('Array size')
 plt.ylabel('Solution size')
+plt.title("Solution size depending on array size")
 plt.show()
 
